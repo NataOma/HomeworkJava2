19d4c20d9c3645d5c1dd1ce91d377c8e1a4eba4e
Natalia <chekiska.11@mail.ru>
2024-10-08 12:01:42 +0500

    Задача4: Сдвиг очереди

---
+++ DequeTask.java
@@ -0,0 +1,32 @@
+package HomeWork4;
+
+import java.util.Deque;
+
+/*
+Задача 4. Сдвиг очереди
+Реализуйте метод rotateDeque в классе DequeTasks, который принимает
+Deque<Integer> и число n. Метод должен повернуть очередь вправо на n
+позиций. Если n отрицательное, повернуть влево.
+ */
+public class DequeTask {
+    public void rotateDeque(Deque<Integer> deq, int n){
+        n = n % deq.size();
+        if (n>0){
+            for (int i = 0; i < n; i++) {
+                deq.addLast(deq.removeFirst());
+            }
+        } else if (n<0) {
+            for (int i = 0; i > n; i--) {
+                deq.addFirst(deq.removeLast());
+            }
+        }
+    }
+    /*
+    Реализуйте метод removeAllOccurrences в классе DequeTasks, который
+принимает Deque<String> и строку value. Метод должен удалить все
+вхождения строки value из очереди.
+     */
+    public void removeAllOccurrences(Deque<String> deque, String value){
+        deque.removeIf(n -> (n.equals(value)));
+    }
+}
